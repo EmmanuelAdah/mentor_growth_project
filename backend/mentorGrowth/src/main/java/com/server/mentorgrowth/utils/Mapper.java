dc3f6fe6154dba4648fe2aea86135997e5ed7f65
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2026-01-08 17:33:00 +0100

    feat: mapper

--- backend/mentorGrowth/src/main/java/com/server/mentorgrowth/utils/Mapper.java
+++ backend/mentorGrowth/src/main/java/com/server/mentorgrowth/utils/Mapper.java
@@ -1,10 +1,15 @@
 package com.server.mentorgrowth.utils;
 
+import com.server.mentorgrowth.dtos.requests.PaymentRequest;
 import com.server.mentorgrowth.dtos.requests.UserRequest;
+import com.server.mentorgrowth.dtos.response.PaymentResponse;
 import com.server.mentorgrowth.dtos.response.UserResponse;
 import com.server.mentorgrowth.exceptions.InvalidRoleException;
+import com.server.mentorgrowth.models.Payment;
 import com.server.mentorgrowth.models.Role;
 import com.server.mentorgrowth.models.User;
+
+import java.util.Map;
 
 public class Mapper {
 
@@ -32,7 +37,37 @@ public class Mapper {
         userResponse.setFirstName(user.getFirstName());
         userResponse.setLastName(user.getLastName());
         userResponse.setEmail(user.getEmail());
+        userResponse.setRole(user.getRole().name());
+        userResponse.setCreatedAt(user.getCreatedAt());
 
         return userResponse;
+    }
+
+    public static Payment mapPayment(String mentorId,
+                              String menteeId,
+                              PaymentRequest request,
+                              Map<String, Object> map){
+
+        Payment payment = new Payment();
+        payment.setMenteeId(menteeId);
+        payment.setMentorId(mentorId);
+        payment.setAmount(request.getAmount());
+        payment.setCurrency(request.getCurrency());
+        payment.setReference(map.get("reference").toString());
+        payment.setStatus("pending");
+
+        return payment;
+    }
+
+    public static PaymentResponse mapPaymentResponse(Payment payment){
+        PaymentResponse paymentResponse = new PaymentResponse();
+        paymentResponse.setId(payment.getId());
+        paymentResponse.setUserId(payment.getMenteeId());
+        paymentResponse.setMentorId(payment.getMentorId());
+        paymentResponse.setStatus(payment.getStatus());
+        paymentResponse.setAmount(payment.getAmount());
+        paymentResponse.setCurrency(payment.getCurrency());
+
+        return paymentResponse;
     }
 }
