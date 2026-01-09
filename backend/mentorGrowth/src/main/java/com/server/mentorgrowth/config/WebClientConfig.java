78fb7f84efa30daf663ee495b3dc0103e1e0dfe0
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2026-01-09 23:04:32 +0100

    feat: web config

--- backend/mentorGrowth/src/main/java/com/server/mentorgrowth/config/WebClientConfig.java
+++ backend/mentorGrowth/src/main/java/com/server/mentorgrowth/config/WebClientConfig.java
@@ -1,8 +1,14 @@
 package com.server.mentorgrowth.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebClientConfig {
@@ -10,5 +16,19 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         return WebClient.create();
+    }
+
+//    @Bean
+//    public WebMvcConfigurer corsRegistry(CorsRegistry registry) {
+//        return registry.addMapping("/api/v1/**")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+//                .allowedHeaders(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+//                .allowedOrigins("")
+//                .allowCredentials(true);
+//    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
