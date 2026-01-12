6e48780076f5873db82aeed8215503334e7e7979
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2026-01-12 23:48:19 +0100

    feat: peer group

---
+++ backend/mentorGrowth/src/main/java/com/server/mentorgrowth/models/PeerGroup.java
@@ -0,0 +1,38 @@
+package com.server.mentorgrowth.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PeerGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private String description;
+    private String category;
+
+    @ManyToMany
+    @JoinTable(
+            name = "peer_group_members",
+            joinColumns = @JoinColumn(name = "peer_group_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> members;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User creator;
+
+    private LocalDateTime createdAt;
+}
