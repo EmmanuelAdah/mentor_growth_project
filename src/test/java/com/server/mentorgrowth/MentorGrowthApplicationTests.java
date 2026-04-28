2e52e4dd5a38c0c748b3a573e88ac30387342d87
emmanuel-adah <edogbanya02@gmail.com>
2026-04-28 16:27:27 +0100

    test: controller test update
    Co-authored-by: Ayo <olajesuayomide04@gmail.com>

--- src/test/java/com/server/mentorgrowth/MentorGrowthApplicationTests.java
+++ src/test/java/com/server/mentorgrowth/MentorGrowthApplicationTests.java
@@ -1,8 +1,41 @@
-package com.server.mentorgrowth;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MentorGrowthApplicationTests {
-
-}
+//package com.server.mentorgrowth;
+//
+//import com.server.mentorgrowth.services.UserServiceImpl;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MockMvc.*;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import java.util.List;
+//import static org.mockito.Mockito.when;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@SpringBootTest
+//class MentorGrowthApplicationTests {
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Autowired
+//    private UserServiceImpl userService;
+//
+//    @MockBean
+//
+//    @BeforeEach
+//    void setUp() {
+//
+//    }
+//
+//    @Test
+//    void getAllMentees_ShouldReturnListOfMentees() throws Exception {
+//        when(userService.getAllMentees()).thenReturn(List.of());
+//
+//        mockMvc.perform(
+//                get("/api/v1/user/mentees/all")
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
+//}
